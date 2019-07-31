fb14513544714834c6d3677dcd6cb2339d4468a8
ccvey789@gmail.com <cy@localhost.localdomain>
2019-07-31 14:45:55 +0800

    add file

---
+++ test_piano/utils/count_accuracy.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 -*-
+
+import cv2
+import numpy
+import argparse
+import os
+
+data_path = '/home/data/cy/piano_video'
+
+audio_txt = os.path.join(data_path, 'video1_crop.txt')
+real_txt = os.path.join(data_path, 'real_key.txt')
+
+if __name__ =='__main__'
