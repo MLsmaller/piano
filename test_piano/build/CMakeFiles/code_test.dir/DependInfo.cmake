71dc76b0103f2e0114c2e37fa574d0ed12fac13a
ccvey789@gmail.com <cy@localhost.localdomain>
2019-07-31 10:34:43 +0800

    pressed_key

---
+++ test_piano/build/CMakeFiles/code_test.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lj/cy/openpose/piano/test_piano/functions/bwlabel.cpp" "/home/lj/cy/openpose/piano/test_piano/build/CMakeFiles/code_test.dir/functions/bwlabel.cpp.o"
+  "/home/lj/cy/openpose/piano/test_piano/functions/detect_key.cpp" "/home/lj/cy/openpose/piano/test_piano/build/CMakeFiles/code_test.dir/functions/detect_key.cpp.o"
+  "/home/lj/cy/openpose/piano/test_piano/functions/get_keyboard.cpp" "/home/lj/cy/openpose/piano/test_piano/build/CMakeFiles/code_test.dir/functions/get_keyboard.cpp.o"
+  "/home/lj/cy/openpose/piano/test_piano/functions/to_json.cpp" "/home/lj/cy/openpose/piano/test_piano/build/CMakeFiles/code_test.dir/functions/to_json.cpp.o"
+  "/home/lj/cy/openpose/piano/test_piano/functions/utils.cpp" "/home/lj/cy/openpose/piano/test_piano/build/CMakeFiles/code_test.dir/functions/utils.cpp.o"
+  "/home/lj/cy/openpose/piano/test_piano/keyboard.cpp" "/home/lj/cy/openpose/piano/test_piano/build/CMakeFiles/code_test.dir/keyboard.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "../functions/headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
